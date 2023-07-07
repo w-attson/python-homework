e459c153b70129fc932402c753ea8f12876cf6ab
Timothy Watson <tbwatson1993@gmail.com>
2023-07-08 00:53:08 +0800

    PyBank: finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,63 +1,89 @@
 import csv
+import numpy as np
+from pathlib import Path
 
 # Path to collect data from the Resources folder
+budget_data_path = Path("./budget_data.csv")
 
-file = open("budget_data.csv", "r")
-budget_data = list(csv.reader(file, delimiter = ","))
-file.close()
-
-print(budget_data)
-
-# Initiate Variables
-
-total_months = 0
-total = 0
-average_change = 0
-greatest_increase = 0
-greastest_decrease = 0
-
-
-'''
-budget_data = []
-
-# Initiate Variables
-
-total_months = 0
-total = 0
-average_change = 0
-greatest_increase = 0
-greastest_decrease = 0
-
-# Open CSV File
-
-
-
-
-for date, profit_loss in budget_data.items():
-    print(date, profit_loss)
+budget_records = []
+ 
+# Read in the CSV file
+with open(budget_data_path, "r") as csvfile:
+    budget_reader = csv.reader(csvfile, delimiter = ",")
     
-
-
-# Totals
-
-for accounts in budget_data:
+    # Read the header row
+    budget_header = next(budget_reader)
+    #print(budget_header)
     
-    total_months += 1
-    total += accounts
+    # Append the column 'Average' to the header
+    budget_header.append("Average Change")
     
-    print(total_months)
-    print(total)
+    # Append the header to the list of sales
+    budget_records.append(budget_header)
     
+    # Initialise variables
+    total_months = 0
+    total_revenue = 0
+    previous_month = 0
+    monthly_change = 0
+    greatest_decrease = 0
+    greatest_increase = 0
+    delta_list = []
+    
+    # Read each row of data following header 
+    for row in budget_reader:
 
+        # Calculate the totals
+        total_months += 1
+        total_revenue += int(row[1])
+
+        # Calculate the average change
+        monthly_change = int(row[1]) - previous_month
+        previous_month = int(row[1])
+        delta_list += [monthly_change]
+        
+        # Append the average change to row
+        row.append(monthly_change)
+        
+        # Append the row to budget records
+        budget_records.append(row)
+        
+        # Calculate the greatest increase
+        if monthly_change > greatest_increase:
+            greatest_increase = monthly_change
+            greatest_increase_month = row[0]
+        
+        # Calculate the greatest decrease
+        if monthly_change < greatest_decrease:
+            greatest_decrease = monthly_change
+            greatest_decrease_month = row[0]
+        
+
+average_delta = (np.mean(delta_list[1:]))
+average_delta = round(average_delta,2)
+
+# Print the calculations to the terminal
 print()
 print("Financial Analysis")
 print()
 print("-----------------------------------")
-print(f"Total Months: {}")
-print(f"Total: ${}")
-print(f"Average Change: {}")
-print(f"Greatest Increase in Profits: {}")
-print(f"Greatect Decrease in Profits: {}")
+print()
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_revenue}")
+print(f"Average Change: ${average_delta}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 print()
 
-'''
+# Output the calculations to the text file
+output = Path("budget_data_completed.txt")
+
+with open(output, 'w') as file:
+    file.write("Financial Analysis\n")
+    file.write("-----------------------------------\n")
+    file.write(f"Total Months: {total_months}\n")
+    file.write(f"Total: ${total_revenue}\n")
+    file.write(f"Average Change: ${average_delta}\n")
+    file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+    file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n")
+    file.write("\n")
